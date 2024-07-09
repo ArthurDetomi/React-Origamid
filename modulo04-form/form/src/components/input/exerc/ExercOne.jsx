05cb9709caea7f056422d24d2dba3011859935b4
Arthur Detomi <arthurdetomi@gmail.com>
2024-07-09 19:47:16 -0300

    Melhorando forma como foi feito o exericicio

--- modulo04-form/form/src/components/input/exerc/ExercOne.jsx
+++ modulo04-form/form/src/components/input/exerc/ExercOne.jsx
@@ -1,5 +1,53 @@
 import React from "react";
 
+const formFields = [
+  {
+    id: "nome",
+    label: "Nome",
+    type: "text",
+  },
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+  },
+  {
+    id: "senha",
+    label: "Senha",
+    type: "password",
+  },
+  {
+    id: "cep",
+    label: "Cep",
+    type: "text",
+  },
+  {
+    id: "rua",
+    label: "Rua",
+    type: "text",
+  },
+  {
+    id: "numero",
+    label: "Numero",
+    type: "text",
+  },
+  {
+    id: "bairro",
+    label: "Bairro",
+    type: "text",
+  },
+  {
+    id: "cidade",
+    label: "Cidade",
+    type: "text",
+  },
+  {
+    id: "estado",
+    label: "Estado",
+    type: "text",
+  },
+];
+
 const ExercOne = () => {
   const [usuario, setUsuario] = React.useState({
     nome: "",
@@ -50,24 +98,13 @@ const ExercOne = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="nome">Nome:</label>
-      <input id="nome" onChange={handleChange} required type="text" />
-      <label htmlFor="email">E-mail:</label>
-      <input id="email" onChange={handleChange} required type="email" />
-      <label htmlFor="senha">Senha:</label>
-      <input id="senha" onChange={handleChange} required type="password" />
-      <label htmlFor="cep">Cep:</label>
-      <input id="cep" onChange={handleChange} type="text" />
-      <label htmlFor="rua">Rua:</label>
-      <input id="rua" onChange={handleChange} type="text" />
-      <label htmlFor="numero">Número:</label>
-      <input id="numero" onChange={handleChange} type="text" />
-      <label htmlFor="bairro">Bairro:</label>
-      <input id="bairro" onChange={handleChange} type="text" />
-      <label htmlFor="cidade">Cidade:</label>
-      <input id="cidade" onChange={handleChange} type="text" />
-      <label htmlFor="estado">Estado:</label>
-      <input id="estado" onChange={handleChange} type="text" />
+      {formFields.map(({ id, label, type }) => (
+        <div key={id}>
+          <label htmlFor={id}>{label}:</label>
+          <input id={id} onChange={handleChange} required type={type} />
+        </div>
+      ))}
+
       {response && response.ok && <p>Usuário Criado</p>}
       {response && console.log(response)}
       <button>Cadastrar</button>
